250fe5acdb932534185cbdc69ad58ddc9812af71
Aymeric SIMORRE <aymericsimorre@gmail.com>
2023-02-09 13:58:26 +0100

    feat: deleted old project

--- todo_app/src/app/app.component.spec.ts
+++ todo_app/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'To-do-list'`, () => {
+  it(`should have as title 'todo_app'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('To-do-list');
+    expect(app.title).toEqual('todo_app');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('To-do-list app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('todo_app app is running!');
   });
 });
